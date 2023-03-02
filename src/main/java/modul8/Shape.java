26dfe3cbd2ff7a66259274d75a9c2ab53b339535
Kahatana22 <120562156+Kahatana22@users.noreply.github.com>
2023-03-02 15:12:04 +0200

    Update Shape.java

--- src/main/java/modul8/Shape.java
+++ src/main/java/modul8/Shape.java
@@ -6,10 +6,6 @@ public class Shape {
     public Shape(String name) {
         this.name = name;
     }
-
-//    public void setName(String name) {
-//        this.name = name;
-//    }
     public String getName() {
         return name;
     }
