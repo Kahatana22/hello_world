b3d10363d5a836bf630bcb16f00450853cfadc0e
Kahatana22 <120562156+Kahatana22@users.noreply.github.com>
2023-03-04 19:40:51 +0200

    Create Circle.java

---
+++ src/main/java/modul8/Circle.java
@@ -0,0 +1,11 @@
+package main.java.modul8;
+
+public class Circle extends Shape {
+    public Circle(String name) {
+        super(name);
+    }
+    public Circle() {
+        super();
+        setName("Circle");
+    }
+}
