d75ee9534718713264f4019c97fc337c9a35ef47
Kahatana22 <120562156+Kahatana22@users.noreply.github.com>
2023-03-04 19:42:06 +0200

    Create Triangle.java

---
+++ src/main/java/modul8/Triangle.java
@@ -0,0 +1,12 @@
+package main.java.modul8;
+
+public class Triangle extends Shape {
+    public Triangle(String name) {
+        super(name);
+    }
+    public Triangle() {
+        super();
+        setName("Triangle");
+    }
+}
+
