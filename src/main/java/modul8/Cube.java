542133524f82baa2e1b061e344a3b878c52c0191
Kahatana22 <120562156+Kahatana22@users.noreply.github.com>
2023-03-04 19:41:11 +0200

    Create Cube.java

---
+++ src/main/java/modul8/Cube.java
@@ -0,0 +1,11 @@
+package main.java.modul8;
+
+public class Cube extends Shape {
+    public Cube(String name) {
+        super(name);
+    }
+    public Cube() {
+        super();
+        setName("Cube");
+    }
+}
