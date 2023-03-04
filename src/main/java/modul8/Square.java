a2bc2f510576143f82b189f96bc86c19fa536536
Kahatana22 <120562156+Kahatana22@users.noreply.github.com>
2023-03-04 19:41:58 +0200

    Create Square.java

---
+++ src/main/java/modul8/Square.java
@@ -0,0 +1,11 @@
+package main.java.modul8;
+
+public class Square extends Shape {
+    public Square(String name) {
+        super(name);
+    }
+    public Square() {
+        super();
+        setName("Square");
+    }
+}
