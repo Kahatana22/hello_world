54fd512f0a87f0e9fb54b4373661ad617406f1b2
Kahatana22 <120562156+Kahatana22@users.noreply.github.com>
2023-03-04 19:41:28 +0200

    Create Quad.java

---
+++ src/main/java/modul8/Quad.java
@@ -0,0 +1,11 @@
+package main.java.modul8;
+
+class Quad extends Shape {
+    public Quad(String name) {
+        super(name);
+    }
+    public Quad() {
+        super();
+        setName("Quad");
+    }
+}
